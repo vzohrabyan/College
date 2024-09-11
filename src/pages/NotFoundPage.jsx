fabfc433e067b4e66f1ef9564a03b905763345c6
VardanZohrabyan <vardanzohrabyan00@gmail.com>
2024-09-12 00:46:41 +0400

    added not Found Page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import Layout from '../layout/Layout';
-import logo from "../assets/icons/logo.png"
-const ProfessionsPage = () => {
+import logo from '../assets/icons/logo.png';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+
+const NotFoundPage = () => {
+  const { t } = useTranslation();
   return (
     <>
       <Layout>
-        <main>
-            4
+        <main className='notFound'>
+          <div>
+            <h1>4</h1>
             <img src={logo} alt="Logo" />
-            4
+            <h1>4</h1>
+          </div>
+          <span>{t('NotFound')}</span>
+          <Link to="/">{t('main')}</Link>
         </main>
       </Layout>
     </>
   );
 };
 
-export default ProfessionsPage;
+export default NotFoundPage;
